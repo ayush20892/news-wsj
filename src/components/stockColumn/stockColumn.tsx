b504f408ea03bc86425f05a30b10d587c1c53039
ayush20892 <ayush20892@gmail.com>
2021-06-15 17:05:52 +0530

    feat: stock column updated

--- src/components/stockColumn/stockColumn.tsx
+++ src/components/stockColumn/stockColumn.tsx
@@ -7,7 +7,7 @@ export function StockColumn () {
   
   return(
     <div className="stockColumn">
-      
+      <iframe id='st_818425f551bd4224b2785af3aa10203f' frameBorder='0' scrolling='no' width='100%' height='100%' src='https://api.stockdio.com/visualization/financial/charts/v1/MarketOverviewChart?app-key=A70DF516523B438A9E175093BD6A3A94&palette=Financial-Light&title=Market%20Overview&onload=st_818425f551bd4224b2785af3aa10203f'></iframe>
     </div>
   );
 }
